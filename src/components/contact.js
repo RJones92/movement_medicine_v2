c71825dff5e7ddaf06c64af41dcf8fb7988a33cc
Rhys <rhys.jones@paconsulting.com>
2022-04-25 14:04:07 +0100

    add contact form validation handling

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,16 +2,43 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { Alert } from 'react-bootstrap';
+
+const cannotBeBlank = "Cannot be blank";
 
 const ContactForm = () => {
     const [serverState, setServerState] = useState({submitting: false, status: null});
     const [form, setForm] = useState({});
+    const [errors, setErrors] = useState({});
 
     const setField = (field, value) => {
         setForm({
             ...form,
             [field]: value
-        })
+        });
+        removeErrors(field);
+    };
+
+    const findFormErrors = () => {
+        const { name, contactDetail, message } = form;
+        const newErrors = {};
+        if ( !contactDetail || contactDetail === '' ) {
+            newErrors.contactDetail = cannotBeBlank;
+        };
+        if ( !message || message === '' ) {
+            newErrors.message = cannotBeBlank;
+        }
+
+        return newErrors;
+    };
+
+    const removeErrors = (field) => {
+        if ( !!errors[field] ) {
+            setErrors({
+                ...errors,
+                [field]: null
+            });
+        };
     }
 
     const handleServerResponse = (ok, msg, form) => {
@@ -22,43 +49,56 @@ const ContactForm = () => {
     };
 
     const handleOnSubmit = e => {
-        console.log(e);
         e.preventDefault();
-        const form = e.target;
-        setServerState({ submitting: true });
-        axios({
-            method: "post",
-            url: process.env.GATSBY_CONTACT_FORM_ENDPONT,
-            data: new FormData(form)
-        })
-        .then(r => {
-            handleServerResponse(true, "Thanks!", form);
-        })
-        .catch(r => {
-            console.log(r);
-            handleServerResponse(false, r.response.data.error, form);
-        });
+        const newErrors = findFormErrors();
+        if ( Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+        } else {
+            const finalForm = e.target;
+            setServerState({ submitting: true });
+            axios({
+                method: "post",
+                url: process.env.GATSBY_CONTACT_FORM_ENDPONT,
+                data: new FormData(finalForm)
+            })
+            .then(r => {
+                handleServerResponse(true, "Thanks!", finalForm);
+                // show success message
+            })
+            .catch(r => {
+                console.log(r);
+                handleServerResponse(false, r.response.data.error, finalForm);
+            });
+        }
     };
 
     return (
-        <Form onSubmit={handleOnSubmit} method="POST">
+        <Form noValidate onSubmit={handleOnSubmit} method="POST">
             <Form.Group className="mb-3" controlId="formName">
                 <Form.Label>Name</Form.Label>
                 <Form.Control 
                     name="name" 
                     type="text"
                     placeholder="Enter name"
-                    onChange={ e => setField("name", e.target.value) }/>
+                    onChange={ e => setField("name", e.target.value) }
+                    isInvalid={ !!errors.name }/>
+                <Form.Control.Feedback type='invalid'>
+                    { errors.name }
+                </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formContactDetail">
                 <Form.Label>Contact detail</Form.Label>
                 <Form.Control
-                    name="contact-detail"
+                    name="contactDetail"
                     required
                     type="text"
                     placeholder="Enter phone number or email address"
-                    onChange={ e => setField("contact-detail", e.target.value) }/>
+                    onChange={ e => setField("contactDetail", e.target.value) }
+                    isInvalid={ !!errors.contactDetail } />
+                <Form.Control.Feedback type='invalid'>
+                    { errors.contactDetail }
+                </Form.Control.Feedback>
                 <Form.Text muted>Please provide your preferred contact details</Form.Text>
             </Form.Group>
 
@@ -70,7 +110,11 @@ const ContactForm = () => {
                     as="textarea"
                     rows="4"
                     placeholder="Enter message"
-                    onChange={ e => setField("message", e.target.value) }/>
+                    onChange={ e => setField("message", e.target.value) }
+                    isInvalid={ !!errors.message }/>
+                <Form.Control.Feedback type='invalid'>
+                    { errors.message }
+                </Form.Control.Feedback>
                 <Form.Text muted>Send us your details and/or preferable booking time and we'll get back to you</Form.Text>
             </Form.Group>
             
