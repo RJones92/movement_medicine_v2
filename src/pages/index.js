85f02da5924039b62a79bd371278ab61529508a9
Rhys <rhys.jones@paconsulting.com>
2022-04-27 09:51:03 +0100

    fix location section mobile view

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,8 +53,8 @@ const IndexPage = () => {
 
         <Section id="location" bgcolor="bg-white">
           <TitleRow text="Find Us" />
-          <Row>
-            <Col>
+          <Row className="gy-5">
+            <Col xs="12" lg="6">
               <a href="https://www.google.com/maps/place/Duncan+Court,+22+Pallister+Rd,+Clacton-on-Sea+CO15+1PG/@51.7886192,1.1538294,17z/data=!3m1!4b1!4m5!3m4!1s0x47d96ccc0704389f:0x1fad504667380c74!8m2!3d51.7886192!4d1.1560181">
                 <StaticImage 
                 src="../images/map.png" 
@@ -63,8 +63,8 @@ const IndexPage = () => {
                 placeholder="blurred" />
               </a>
             </Col>
-            <Col>
-            <Row className="pb-5">
+            <Col xs="12" lg="6">
+              <Row className="pb-5">
                 <Col xs="2">
                   <FontAwesomeIcon icon={faMapMarkerAlt} size="3x" />
                 </Col>
