ff11fa92dfa0a8702ba445a25567fbcf2dacaaec
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2022-10-11 20:42:22 +0100

    update the footer with new image pulled right and made small

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,18 +22,29 @@ const Layout = ({
           contactUsButtonVisible={contactUsButtonVisible}
         />
         {children}
-        <Section id='footer' bgcolor='bg-secondary'>
-          <Row xs='2' md='3'>
-            <Col className='my-3'>
-              <StaticImage
-                src='../images/Im_Registered_Mark_10013.jpg'
-                alt="I'm Registered Mark, 10013"
-                layout='constrained'
-                placeholder='blurred'
-              />
-            </Col>
-          </Row>
-        </Section>
+
+        <Row id='footer' className={`justify-content-center py-3 bg-secondary`}>
+          <Col>
+            <section>
+              <Container>
+                <Row className='justify-content-center align-items-center'>
+                  <Col xs='12' md='10' lg='8'>
+                    <Row xs='5' md='5' lg='6' className='justify-content-end'>
+                      <Col>
+                        <StaticImage
+                          src='../images/Im_Registered_Mark_10013.jpg'
+                          alt="I'm Registered Mark, 10013"
+                          layout='constrained'
+                          placeholder='blurred'
+                        />
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+              </Container>
+            </section>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
