82c63bb12bc07d76815fd7180a68a40bbc848502
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2022-11-14 20:34:06 +0000

    add faq text and accordion

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -29,7 +29,7 @@ export default function NavigationBar() {
                 className='nav-link'
                 activeClassName='active'
               >
-                FAQ
+                FAQs
               </Link>
             </Nav.Item>
             <Nav.Item as='li' eventKey='/#services'>
