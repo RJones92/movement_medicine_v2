a4ea9e2006298200d3b5c33ff0b95f489470730a
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2022-12-19 21:16:14 +0000

    update 'find us' section to have buildign image and clickable links for contact details

--- src/pages/index.js
+++ src/pages/index.js
@@ -130,33 +130,53 @@ const IndexPage = () => {
           <TitleRow text='Find Us' />
           <Row className='gy-5'>
             <Col xs='12' lg='6'>
-              <a href='https://www.google.com/maps/place/Duncan+Court,+22+Pallister+Rd,+Clacton-on-Sea+CO15+1PG/@51.7886192,1.1538294,17z/data=!3m1!4b1!4m5!3m4!1s0x47d96ccc0704389f:0x1fad504667380c74!8m2!3d51.7886192!4d1.1560181'>
-                <StaticImage
-                  src='../images/map.png'
-                  alt='map'
-                  layout='constrained'
-                  placeholder='blurred'
-                />
-              </a>
+              <StaticImage
+                src='../images/building.jpg'
+                alt='map'
+                layout='constrained'
+                placeholder='blurred'
+              />
             </Col>
             <Col xs='12' lg='6'>
-              <Row className='pb-5'>
+              <Row className='pb-3'>
+                <Col>
+                  <a href='https://www.google.com/maps/place/Duncan+Court,+22+Pallister+Rd,+Clacton-on-Sea+CO15+1PG/@51.7886192,1.1538294,17z/data=!3m1!4b1!4m5!3m4!1s0x47d96ccc0704389f:0x1fad504667380c74!8m2!3d51.7886192!4d1.1560181'>
+                    <StaticImage
+                      src='../images/map.png'
+                      alt='map'
+                      layout='constrained'
+                      placeholder='blurred'
+                    />
+                  </a>
+                </Col>
+              </Row>
+              <Row className='pb-3'>
                 <Col xs='2'>
                   <FontAwesomeIcon icon={faMapMarkerAlt} size='3x' />
                 </Col>
-                <Col xs='10'>22 Pallister Road, Clacton-on-Sea, CO15 1PG</Col>
+                <Col xs='10'>
+                  <a href='https://www.google.com/maps/place/Duncan+Court,+22+Pallister+Rd,+Clacton-on-Sea+CO15+1PG/@51.7886192,1.1538294,17z/data=!3m1!4b1!4m5!3m4!1s0x47d96ccc0704389f:0x1fad504667380c74!8m2!3d51.7886192!4d1.1560181'>
+                    22 Pallister Road, Clacton-on-Sea, CO15 1PG
+                  </a>
+                </Col>
               </Row>
-              <Row className='pb-5'>
+              <Row className='pb-3'>
                 <Col xs='2'>
                   <FontAwesomeIcon icon={faPhone} size='3x' />
                 </Col>
-                <Col xs='10'>07922 448 380</Col>
+                <Col xs='10'>
+                  <a href='tel:+07922448380'>07922 448 380</a>
+                </Col>
               </Row>
-              <Row className='pb-5'>
+              <Row className='pb-3'>
                 <Col xs='2'>
                   <FontAwesomeIcon icon={faEnvelope} size='3x' />
                 </Col>
-                <Col>anemailaddress@gmail.com</Col>
+                <Col>
+                  <a href='mailto:anemailaddress@gmail.com'>
+                    anemailaddress@gmail.com
+                  </a>
+                </Col>
               </Row>
             </Col>
           </Row>
