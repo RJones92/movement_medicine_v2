e4234121c7301d78dedfc314ae35dd431334b14e
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2023-01-31 20:57:25 +0000

    Open google map link on new tab

--- src/components/contactWidget.js
+++ src/components/contactWidget.js
@@ -1,13 +1,22 @@
 import * as React from 'react';
 import { Col, Row } from 'react-bootstrap';
 
-const ContactWidget = ({ widgetIcon, widgetText, widgetUrl }) => {
+const ContactWidget = ({ widgetIcon, widgetText, widgetUrl, newTab }) => {
+  let textEl;
+  if (newTab) {
+    textEl = (
+      <a href={widgetUrl} target='_blank' rel='noreferrer'>
+        {widgetText}
+      </a>
+    );
+  } else {
+    textEl = <a href={widgetUrl}>{widgetText}</a>;
+  }
+
   return (
     <Row className='pb-3 align-items-center'>
       <Col xs='2'>{widgetIcon}</Col>
-      <Col xs='10'>
-        {widgetUrl ? <a href={widgetUrl}>{widgetText}</a> : { widgetText }}
-      </Col>
+      <Col xs='10'>{textEl}</Col>
     </Row>
   );
 };
