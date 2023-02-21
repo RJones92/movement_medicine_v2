0dcac03875e1b0900372788f2e776bf6cb91d62f
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2023-02-21 14:40:19 +0000

    insert 'about me' content

--- src/pages/aboutMe.js
+++ src/pages/aboutMe.js
@@ -7,32 +7,73 @@ import Col from 'react-bootstrap/Col';
 const AboutMePage = () => {
   return (
     <Service bannerTitle='About Me' id='about-me' bgcolor='bg-white'>
-      <Row className='gy-5'>
-        <Col xs='12' lg='6'>
-          <p class='lead'>
-            <i>
-              An experienced professional in aiding mobility and relieving
-              muscoskeletal pain
-            </i>
+      <Row className='my-3'>
+        <Col>
+          <p class='lead'>Hi, I'm Jamie.</p>
+          <p align='justify'>
+            I became an Osteopath in 2018, finishing with distinction in the
+            final clinical competency examinations. Since then, I have worked at
+            Clacton and in several other clinics to gain as much experience and
+            knowledge as possible. I have always taken an interest in the human
+            body, and was eager to learn how the body functions.{' '}
           </p>
-          <hr />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+          <p align='justify'>
+            I adapt my approach when assessing and treating patients based on a
+            variety of factors. What helps one person's pain, may not work for
+            others. No two patients ever respond in the same way - even if they
+            have the same injury!
           </p>
         </Col>
-        <Col xs='12' lg='6'>
+      </Row>
+      <Row className='my-3 justify-content-center'>
+        <Col xs='12' md='8'>
+          <p>
+            I utilise a variety of skills and treatments, such as:
+            <ul>
+              <li>Osteopathic Manipulation</li>
+              <li>Soft tissue techniques</li>
+              <li>Sports massage, </li>
+              <li>Dry needling (medical acupuncture)</li>
+              <li>Exercise prescription</li>
+              <li>CBT (in relation to pain)</li>
+              <li>Therapeutic ultrasound</li>
+              <li>Electrotherapy</li>
+              <li>
+                I am soon taking a course in Cognitive Functional Therapy.
+              </li>
+            </ul>
+          </p>
+        </Col>
+        <Col xs='6' md='4'>
           <StaticImage
             src='../images/face.jpg'
             alt='face'
             layout='constrained'
             placeholder='blurred'
           ></StaticImage>
+        </Col>
+      </Row>
+      <Row className='my-3'>
+        <Col>
+          <p align='justify'>
+            All of these skills combined with an in-depth knowledge of the human
+            body gives me an excellent chance at being able to help patients
+            with their problems. Of course, I also know my limitations, and
+            sadly some patients do not respond to manual therapy. In these
+            cases, I will always write to GPs and can suggest appropriate
+            referrals when indicated.
+          </p>
+          <p align='justify'>
+            I will always aim to gradually increase the loading on the injured
+            or painful tissue so that there is a steady and safe return to the
+            patient's previous strength and ability. This may be done by
+            adapting elements of their work or home life, or by progressive
+            exercises which I am able to send out with videos and pictures of
+            for patients' convenience.
+          </p>
+          <p align='justify'>
+            I look forward to meeting and helping you in clinic soon.
+          </p>
         </Col>
       </Row>
     </Service>
