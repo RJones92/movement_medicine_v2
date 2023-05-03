de1ad0292abf1edc339aba235a70e127f5b26e82
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2023-05-03 20:45:56 +0100

    Revert "Update services descriptions"
    This reverts commit f4ca54ddc955f05df71e34bf45d06c28ba893df0.

--- src/pages/services/backpain.js
+++ src/pages/services/backpain.js
@@ -10,12 +10,22 @@ const BackPainPage = () => {
       bgcolor='bg-white'
     >
       <p>
-        Back pain can be debilitating and affect your daily life. Whether caused
-        by an injury, poor posture, or a medical condition, it's important to
-        address the issue to prevent it from worsening. At Clacton COP Clinic we
-        can help diagnose and treat the root cause of your back pain, providing
-        relief and improving your quality of life. Contact us today to learn
-        more about our effective and personalised back pain treatments.
+        Back pain and Neck pain combined is the most common reason to take time
+        off work due to a physical disorder. Six out of ten people in the UK
+        will at some stage suffer with some form of low back pain. In most cases
+        acute back pain will resolve within six weeks, however it is not
+        uncommon for back pain to persist and become chronic. A person's spine
+        is central to our bodies and integral to all movement and activity.
+        People often do not realise how many daily tasks can become intolerable
+        when they are experiencing back or neck pain.
+      </p>
+      <p>
+        Following a thorough assessment to discern to cause of your back pain,
+        treatment will consist of gentle to firm stretching, deep tissue massage
+        and the use of various treatment modalities which we have use of within
+        the clinic to help lower your symptoms. We will also find the correct
+        exercises which you can do, to progressively and safely begin to load
+        your spine "back" to full strength.
       </p>
     </Service>
   );
--- src/pages/services/injuryrehab.js
+++ src/pages/services/injuryrehab.js
@@ -10,14 +10,29 @@ const InjuryRehabPage = () => {
       bgcolor='bg-white'
     >
       <p>
-        Injury rehabilitation can be a challenging process, but our
-        physiotherapy clinic is here to help. We offer personalised treatments
-        and exercise programs to help you recover from injury and get back to
-        your daily life. Whether you're recovering from a sports injury or a
-        joint replacement surgery, we utilise the latest techniques and
-        therapies to help restore function, alleviate pain, and prevent further
-        injury. Contact us today to schedule an appointment and take the first
-        step towards a full recovery.
+        There are hundreds of ways people can hurt themselves. Common causes are
+        from falls, a sports-related incident, lifting something a little too
+        heavy, or simply overdoing an activity which the body is not accustomed
+        to. Whatever the reason, there will be an injured tissue to heal and
+        restrengthen. It is important for a physical therapist to assess which
+        tissue you have hurt (a muscle, ligament, tendon etc), so that you can
+        know the correct treatments you should be undertaking to help it, and
+        how long you can expect the recovery to take. An assessment will also
+        help to answer the following questions, and help you to get your body
+        healing as quickly as possible:
+      </p>
+      <p>
+        <ul>
+          <li>Is it safe to carry on using the affected part of your body?</li>
+          <li>Should you use hot or cold packs?</li>
+          <li>Should you take painkillers?</li>
+          <li>Could you continue to go to work?</li>
+          <li>Should I stretch it, or rest it?</li>
+        </ul>
+      </p>
+      <p>
+        These are all questions which following an assessment, we can answer for
+        you, and get your body healing as quickly as possible.
       </p>
     </Service>
   );
