8a942a025c65899af85b2598427e6d15c7bca6d8
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2026-02-25 20:17:18 +0000

    WIP embed iframe booking

--- src/components/service.js
+++ src/components/service.js
@@ -16,6 +16,21 @@ const Service = ({ id, bgcolor, bannerTitle, children }) => {
           <Row className='justify-content-center pt-3'>
             <Col xs='auto'>
               <ContactNowButtons />
+              <div>
+                {/* To make the iframe work with ReactJS, needed to:
+              1. frameborder -> frameBorder
+              2. style had to be an object
+              3. add a title (react requirement) */}
+                {/* TODO probably have to fiddle with the height/width */}
+                <iframe
+                  src='https://bookings.rushcliff.com/?system=445087'
+                  allow='payment'
+                  sandbox='allow-forms allow-same-origin allow-scripts allow-popups allow-top-navigation allow-top-navigation-by-user-activation allow-storage-access-by-user-activation'
+                  frameBorder='0'
+                  style={{ height: '100%', width: '100%' }}
+                  title='Booking System'
+                ></iframe>
+              </div>
             </Col>
           </Row>
         </Section>
